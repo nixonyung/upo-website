95c8c8b165705ff4cbe96bbc607d0b182bf4716b
Nixon Yung <nixonyung123@gmail.com>
2022-06-05 00:32:39 +0800

    Navbar Logo use select-none

--- src/components/Navbar/StyledNavLogo.tsx
+++ src/components/Navbar/StyledNavLogo.tsx
@@ -5,7 +5,7 @@ import Logo from '../../images/logo.png'
 export default function StyledNavLogo() {
   return (
     <Link to='/#'>
-      <div className='flex items-center'>
+      <div className='flex items-center select-none'>
         <Image src={Logo} height={40} />
         <span className='font-Mont ml-2 text-lg'>UST</span>
         <span className='font-Mont tracking-widerrr ml-3 text-lg font-bold'>
