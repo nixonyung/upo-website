1c01a2b05d21cb72b268a3578ffb2e769453d228
Nixon Yung <nixonyung123@gmail.com>
2022-09-01 21:38:12 +0800

    Update news

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -3,6 +3,7 @@ import {
   faMusic,
   faPeopleGroup,
   faPlayCircle,
+  faVideo,
 } from '@fortawesome/free-solid-svg-icons';
 import {Image} from '@mantine/core';
 import {useEffect, useRef} from 'react';
@@ -13,7 +14,8 @@ import NewsItem from '../components/NewsItem';
 import Section from '../components/Section';
 import cabGroupJPG from '../images/cab-group.webp';
 import jointUConcertJpg from '../images/news-JointUConcert.jpg';
-import miniConcertPng from '../images/news-MiniConcert.png';
+import miniConcertJpg from '../images/news-MiniConcert.jpg';
+import mvpPng from '../images/news-MVP.png';
 import orchJPG from '../images/orch.webp';
 
 export default function Index() {
@@ -40,7 +42,7 @@ export default function Index() {
           subheading={<p>Show on 6, 7 September</p>}
           icon={faMusic}
         >
-          <Image src={jointUConcertJpg} width={300} />
+          <Image src={jointUConcertJpg} width={350} />
           <br />
           To mark the beginning of the new academic year, UPO of HKU, HKUSTSU and PolyU SAOPhil
           proudly present Joint University Performance 2022. This concert will present various forms
@@ -106,13 +108,56 @@ export default function Index() {
           .
         </NewsItem>
 
+        <NewsItem
+          heading="Music Video Project"
+          subheading="Registration opens until 28 Feb 2023"
+          icon={faVideo}
+        >
+          <Image src={mvpPng} width={400} />
+          <br />
+          Music video project perform in an ensemble, solo, or even a one-man ensemble for those who
+          play multiple instruments.
+          <br />
+          <br />
+          <u>Format</u>: Pre-recorded videos of your performance will be edited and uploaded to
+          social media.
+          <br />
+          <u>Target participants</u>: All UPO members including
+          <ul className="list-decimal list-inside">
+            <li>String players</li>
+            <li>Woodwind &amp; Brass players</li>
+            <li>Percussionists</li>
+            <li>Pianists</li>
+          </ul>
+          <u>Formations</u>: Solo, ensemble up to 4 players, one-man ensemble
+          <br />
+          <u>Pieces to perform</u>: YOUR CHOICE!
+          <br />
+          <u>Application period</u>: Now until 28 Feb 2023
+          <br />
+          <br />
+          Click&nbsp;
+          <a
+            href="https://forms.gle/WG2VF8jvFjsFsquh6"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-400 underline"
+          >
+            here
+          </a>
+          &nbsp;to register.
+          <br />
+          <br />
+          We are looking forward to seeing your wonderful performance on the web!
+        </NewsItem>
+
         <NewsItem heading="Mini Concert" subheading="Show on 5 October" icon={faMasksTheater}>
-          <Image src={miniConcertPng} width={300} />
+          <Image src={miniConcertJpg} width={550} />
           <br />
           Our brand new activity featuring solos, duets, and ensembles.
           <br />
           <br />
-          <u>Details:</u>
+          Details:
           <br />
           Date:&nbsp;&nbsp;&nbsp;&nbsp;5/10/2022
           <br />
@@ -120,6 +165,19 @@ export default function Index() {
           <br />
           Venue:&nbsp;&nbsp;Main Hall, Shaw Auditorium
           <br />
+          <br />
+          Repertoire:
+          <br />
+          <ul className="list-decimal list-inside">
+            <li>Grieg Violin Sonata No. 3 in C Minor, Op. 45, 1st Movement</li>
+            <li>J. J. Quantz Flute Duet in D Major, Op. 2, No. 5</li>
+            <li>Herman Beeftink - Summer</li>
+            <li>Liszt Transcendental Etude No. 10</li>
+            <li>Rossini Gioacchino Duet for Cello and Double Bass in D Major 1st Movement</li>
+            <li>Bach Cello Suite No. 1 Prelude</li>
+            <li>Bach-Busoni Chaconne in D Major</li>
+            <li>Faure - Piano Quartet No. 1 in C Minor, Op. 15</li>
+          </ul>
           <br />
           Please contact Summerloretta (98427158) for any enquiries.
         </NewsItem>
